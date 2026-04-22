2f02138ae926d0d95cdceb85ff2230bb5d8b6bb2
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-04-22 13:01:22 +0200

    Minor documentation tweak

--- src/Opal/Native/Windows/StandardDevice.cs
+++ src/Opal/Native/Windows/StandardDevice.cs
@@ -1,7 +1,7 @@
 ﻿namespace Opal.Native.Windows;
 
 /// <summary>
-/// The standard console devices.
+/// The standard console devices (standard streams).
 /// </summary>
 /// <remarks>
 /// Documentation: <see href="https://learn.microsoft.com/en-us/windows/console/getstdhandle"/>
@@ -9,17 +9,17 @@
 internal enum StandardDevice
 {
     /// <summary>
-    /// The standard input device.
+    /// The standard input device (<c>stdin</c>).
     /// </summary>
     STD_INPUT_HANDLE = -10,
 
     /// <summary>
-    /// The standard output device.
+    /// The standard output device (<c>stdout</c>).
     /// </summary>
     STD_OUTPUT_HANDLE = -11,
 
     /// <summary>
-    /// The standard error device.
+    /// The standard error device (<c>stderr</c>).
     /// </summary>
     STD_ERROR_HANDLE = -12
 }
